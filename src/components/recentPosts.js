3ed1903fca8d49f91030cb94425beb699278f84c
paulaluina <paula_lf@yahoo.es>
2024-08-28 19:58:35 +0200

    set up results component

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -6,14 +6,14 @@ class RecentPosts extends Component {
             <div className="recent-posts">
                 <div className="recent-posts__wrapper">
                     <div className="recent-posts__heading">Recent Posts</div>
-                    <ul className="recent-posts__posts">    
-                        <li>recent post 0</li>  
-                        <li>recent post 1</li>
-                        <li>recent post 2</li>  
+                    <ul className="recent-posts__posts">
+                    <li>recent post 0</li>
+                    <li>recent post 1</li>
+                    <li>recent post 2</li>
                     </ul>
                 </div>
             </div>
-        );
+        )
     }
 }
 
