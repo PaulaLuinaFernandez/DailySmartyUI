4a95e7da11153d989121521aedabf939d82e7aee
paulaluina <paula_lf@yahoo.es>
2024-08-28 21:59:08 +0200

    added exact route for home route and implemented withRouter HOC into the searchbar to navigate the user to results route

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -7,9 +7,9 @@ class RecentPosts extends Component {
                 <div className="recent-posts__wrapper">
                     <div className="recent-posts__heading">Recent Posts</div>
                     <ul className="recent-posts__posts">
-                    <li>recent post 0</li>
-                    <li>recent post 1</li>
-                    <li>recent post 2</li>
+                        <li>recent post 0</li>
+                        <li>recent post 1</li>
+                        <li>recent post 2</li>
                     </ul>
                 </div>
             </div>
