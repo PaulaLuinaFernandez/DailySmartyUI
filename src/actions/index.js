b2b164193f16c49e79df833c7355d63cfd95c4e4
paulaluina <paula_lf@yahoo.es>
2024-08-30 19:28:48 +0200

    setup redux devtools and fixed reducer for set_recent_posts

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import { SET_RECENT_POSTS } from './types';
+
+import axios from 'axios'
+
+export function fetchRecentPosts() {
+    return function(dispatch) {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            console.log(response.data);
+            dispatch({
+                type: SET_RECENT_POSTS,
+                payload: response.data.posts
+            })
+        })
+    }
+}
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,30 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import {thunk} from 'redux-thunk';
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(compose((window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f)(createStore)));
+
 
 import "./style/main.scss";
 
-import Home from "./components/home";
-import Results from "./components/results";
+import Home from './components/home';
+import Results from './components/results';
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/results" component={Results}/>
+          <Route path='/' component={Home}/>
+          <Route path='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
-    </Provider>,
-    document.querySelector(".app-wrapper")
+    </Provider>
+    , document.querySelector('.app-wrapper')
   );
 }
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
